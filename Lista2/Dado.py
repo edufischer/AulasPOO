fa5e8003c364e828bc5c2604fe98f4b7f762477c
Eduardo <eduardodecastrofischer@gmail.com>
2023-09-14 20:25:37 -0300

    atualizando codigos

--- Lista2/Dado.py
+++ Lista2/Dado.py
@@ -38,20 +38,3 @@ dado_com_6_lados = Dado(6)
 dado_com_8_lados = Dado(8)
 dado_com_12_lados = Dado(12)
 
-#print(f'Rolando dado de 6 lados... O resultado foi: {dado_com_6_lados.Rolar()}\n')
-#print(f'Rolando dado de 6 lados... O resultado foi: {dado_com_6_lados.Rolar()}\n')
-#print(f'Rolando dado de 6 lados... O resultado foi: {dado_com_6_lados.Rolar()}\n')
-#
-#print(f'Rolando dado de 8 lados... O resultado foi: {dado_com_8_lados.Rolar()}\n')
-#print(f'Rolando dado de 8 lados... O resultado foi: {dado_com_8_lados.Rolar()}\n')
-#print(f'Rolando dado de 8 lados... O resultado foi: {dado_com_8_lados.Rolar()}\n')
-#
-#print(f'Rolando dado de 12 lados... O resultado foi: {dado_com_12_lados.Rolar()}\n')
-#print(f'Rolando dado de 12 lados... O resultado foi: {dado_com_12_lados.Rolar()}\n')
-#print(f'Rolando dado de 12 lados... O resultado foi: {dado_com_12_lados.Rolar()}\n')
-
-#    Faça um programa que simule um "dado virtual". O dado deve ser modelado como uma classe,
-#possuindo apenas o número de faces e o método Rolar, que retorna o valor sorteado. O número
-#de faces deve ser definido na criação do objeto (construtor com parâmetro). Deve ser instanciado
-#um dado com 6, 8 e 12 faces no main(). Cada dado deve ser jogado 3 vezes e o resultado de cada
-#jogada deve ser impresso na tela. Não deve ser usado print dentro da classe.
