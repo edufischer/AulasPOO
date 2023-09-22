55d94b643bc36fcc099c0e5f2d65e3c951e3cf9a
edufischer <eduardodecastrofischer@gmail.com>
2023-09-21 21:57:18 -0300

    Update ArgumentosVariados.py

--- Others/ArgumentosVariados.py
+++ Others/ArgumentosVariados.py
@@ -1,3 +1,22 @@
+""" o uso de **kwargs em uma função permite que você trate os argumentos passados como um dicionário dentro da função.
+ kwargs é uma abreviação de "keyword arguments" (argumentos de palavras-chave) e permite que você passe argumentos nomeados 
+ arbitrários para uma função. Dentro da função, você pode acessar esses argumentos nomeados como se fossem pares chave-valor em um dicionário.
+
+Aqui está um exemplo que ilustra como **kwargs funciona como um dicionário:
+
+def mostrar_info(**kwargs):
+    for chave, valor in kwargs.items():
+        print(f'{chave}: {valor}')
+
+mostrar_info(nome='Alice', idade=30, cidade='São Paulo')
+Neste exemplo, os argumentos nome, idade e cidade são passados para a função mostrar_info usando **kwargs. Dentro da função, esses
+ argumentos são tratados como um dicionário, onde as chaves são os nomes dos argumentos e os valores são os valores associados a essas
+   chaves. A função itera pelo dicionário kwargs e imprime as informações associadas a cada chave.
+
+Portanto, **kwargs é uma maneira conveniente de lidar com argumentos nomeados arbitrários em Python e permite que você trate esses
+ argumentos como um dicionário dentro da função. """
+
+
 def function_doubleasterix(**keywordargs):
 
     print("The KEYS in the kwargs dicionary are -", keywordargs.keys())
