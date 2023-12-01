491e4b0ac407d8d976c875f4cc6f1d20ee43b977
edufischer <eduardodecastrofischer@gmail.com>
2023-12-01 10:22:47 -0300

    last commit
    finally

--- Lista8/questao_1.py
+++ Lista8/questao_1.py
@@ -1,3 +1,5 @@
+import random
+
 # Classe base Animal
 class Animal:
 
@@ -31,7 +33,7 @@ class Herbivoro(Animal):
 
 
 # Classe derivada Onivoro
-class Onivoro(Animal):
+class Onivoro(Carnivoro, Herbivoro):
 
     def comer(self):
         numero = random.randint(0, 1)
@@ -43,6 +45,8 @@ class Onivoro(Animal):
     def exibirDescricao(self):
         super().exibirDescricao()
         print(f"O animal {self.nome} é um animal onívoro.")
+
+
 
 
 # Exemplo de uso
@@ -62,54 +66,3 @@ onivoro.exibirDescricao()  # O animal Urso é um animal onívoro.
 onivoro.comer()  # O animal Urso está caçando.
 
 
-## ====================================================================================================================
-
-import random
-
-class Animal:
-    def __init__(self, nome):
-        self.nome = nome
-
-    def exibirDescricao(self):
-        print(f"Nome do animal: {self.nome}")
-
-class Carnivoro(Animal):
-    def cacar(self):
-        print(f"{self.nome} está caçando.")
-
-    def exibirDescricao(self):
-        super().exibirDescricao()
-        print("É um animal carnívoro.")
-
-class Herbivoro(Animal):
-    def pastar(self):
-        print(f"{self.nome} está pastando.")
-
-    def exibirDescricao(self):
-        super().exibirDescricao()
-        print("É um animal herbívoro.")
-
-class Onivoro(Carnivoro, Herbivoro):
-    def comer(self):
-        escolha = random.randint(0, 1)
-        if escolha == 0:
-            self.cacar()
-        else:
-            self.pastar()
-
-    def exibirDescricao(self):
-        super().exibirDescricao()
-        print("É um animal onívoro.")
-
-# Exemplo de uso
-animal_carnivoro = Carnivoro("Leão")
-animal_carnivoro.exibirDescricao()
-animal_carnivoro.cacar()
-
-animal_herbivoro = Herbivoro("Zebra")
-animal_herbivoro.exibirDescricao()
-animal_herbivoro.pastar()
-
-animal_onivoro = Onivoro("Urso")
-animal_onivoro.exibirDescricao()
-animal_onivoro.comer()
